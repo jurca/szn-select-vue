a94fc69bf8561d329dd55f3bf18aaa60a1cf334f
Martin Jurča <martin.jurca@firma.seznam.cz>
2018-02-27 13:45:49 +0100

    embedded the embeddable loader

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 'use strict' // eslint-disable-line strict
 
 const del = require('del')
+const fs = require('fs')
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 const replace = require('gulp-replace')
+const util = require('util')
 const packageInfo = require('./package.json')
 
 const compatibleVersionRange = `${packageInfo.version.split('.').shift()}.x`
@@ -17,8 +19,26 @@ function copyMetaFiles() {
     .pipe(gulp.dest('./dist'))
 }
 
+async function injectLoader(done) {
+  const readFile = fileName => util.promisify(fs.readFile)(fileName, 'utf-8')
+  const writeFile = (fileName, contents) => util.promisify(fs.writeFile)(fileName, contents, 'utf-8')
+
+  const [loader, component] = await Promise.all([
+    require.resolve('@jurca/szn-select/embeddableLoader.js'),
+    './index.js',
+  ].map(readFile))
+
+  try {
+    await util.promisify(fs.mkdir)('./dist', 0o775)
+  } catch (_) {}
+
+  writeFile('./dist/index.js', component.replace('// %{EMBEDDABLE_LOADER}%', loader))
+
+  done()
+}
+
 function compileJs() {
-  return gulp.src('./index.js')
+  return gulp.src('./dist/index.js')
     .pipe(babel({
       presets: [['env', {
         targets: {
@@ -34,6 +54,9 @@ exports.default = gulp.series(
   clean,
   gulp.parallel(
     copyMetaFiles,
-    compileJs,
+    gulp.series(
+      injectLoader,
+      compileJs,
+    ),
   ),
 )
